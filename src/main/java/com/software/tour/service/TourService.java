4a0856e785ed380e38462acfe207a000f8c9356f
ngochivinh <vnngochivinh@gmail.com>
2013-11-21 15:43:21 +0700

    343

--- src/main/java/com/software/tour/service/TourService.java
+++ src/main/java/com/software/tour/service/TourService.java
@@ -3,6 +3,9 @@ package com.software.tour.service;
 import java.util.List;
 
 import com.software.tour.domain.Tour;
+import com.software.tour.util.SearchDTO;
+import com.software.tour.util.MyPageRequest;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -10,6 +13,9 @@ public interface TourService {
 	public List<Tour> findAll();
 	public Tour findById(Long id);
 	public Tour save(Tour tour);
-	//public void delete(@org.springframework.data.repository.query.Param("Id")Long id);
+	public void delete(Tour tour);
 	public Page<Tour> findAllByPage(Pageable pageable);
+	public List<Tour> search(SearchDTO dto);
+	//public List<Tour>findAllForPage(int pageIndex, int pageSize);
+	public Page<Tour> search(MyPageRequest pageable);
 }
